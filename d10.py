92305f05202e2b7badc541c3017fabba3e7b891a
JasperGeurtz <jcgeurtz@gmail.com>
2020-12-10 18:56:32 +0100

    use cache to calc 10.2

--- d10.py
+++ d10.py
@@ -12,27 +12,21 @@ for i in range(1, len(ms)):
 
 print("1:", diffs[1] * diffs[3])
 
-lms = len(ms)
-print(ms)
-mt = 0
-def perms(i):#, ll):
-    if i == lms - 1:
+
+cache = {}
+
+def perms(i):
+    if i in cache:
+        return cache[i]
+    if i == len(ms) - 1:
         return 1
-    # print("push", i)
     t = 0
     for j in range(i + 1, i + 4):
-        if j < lms and ms[j] <= ms[i] + 3:
+        if j < len(ms) and ms[j] <= ms[i] + 3:
             t += perms(j)
-            # t += perms(j, ll[:] + [ms[j]])
-
         else:
             break
-    # print("pop", i, t, ll)
-    global mt
-    if t > mt:
-        print(mt)
-        mt = t
+    cache[i] = t
     return t
 
 print("2:", perms(0))
-# print(perms(0, [ms[0]]))
