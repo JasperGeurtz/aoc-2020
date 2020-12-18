731e30ac1dd4de5432c07639e8db5284b68c340e
Jasper Geurtz <jcgeurtz@gmail.com>
2020-12-18 14:59:01 +0100

    day18 still ugly

--- d18.py
+++ d18.py
@@ -3,43 +3,38 @@ import utils
 m = utils.opener.lines("input/18.txt")
 
 
-def find_matching_right(expr, i, ):
+def find_matching(expr, i, dir):
+    other = ")" if dir == "(" else "("
+    rge = [i+1, len(expr)] if dir == "(" else [i-1, -1, -1]
     ctr = 1
-    for j in range(i + 1, len(expr)):
-        if expr[j] == "(":
+    for j in range(*rge):
+        if expr[j] == dir:
             ctr += 1
-        elif expr[j] == ")":
+        elif expr[j] == other:
             ctr -= 1
             if ctr == 0:
                 return j
     raise ("error: ", expr, i, expr[i])
-
 
 def calc1(expr):
     t = 0
     i = 0
     while i < len(expr):
         c = expr[i]
-        if c == "+":
+        if c in "+*":
             if expr[i + 1] == "(":
-                mi = find_matching_right(expr, i + 1)
+                mi = find_matching(expr, i + 1, "(")
                 s = calc1(expr[i + 2:mi])
                 i = mi
             else:
                 s = int(expr[i + 1])
                 i += 1
-            t += s
-        elif expr[i] == "*":
-            if expr[i + 1] == "(":
-                mi = find_matching_right(expr, i + 1)
-                s = calc1(expr[i + 2:mi])
-                i = mi
-            else:
-                s = int(expr[i + 1])
-                i += 1
-            t *= s
+            if c == "+":
+                t += s
+            if c == "*":
+                t *= s
         elif c == "(":
-            mi = find_matching_right(expr, i)
+            mi = find_matching(expr, i, "(")
             t += calc1(expr[i + 1:mi])
             i = mi
         else:
@@ -48,22 +43,8 @@ def calc1(expr):
     return t
 
 
-s1 = 0
-for line in m:
-    s1 += calc1(list(line.replace(" ", "")))
+s1 = sum([calc1(list(line.replace(" ", ""))) for line in m])
 print("1: ", s1)
-
-def find_matching_left(expr, i):
-    ctr = 1
-    for j in range(i-1, -1, -1):
-        if expr[j] == ")":
-            ctr += 1
-        elif expr[j] == "(":
-            ctr -= 1
-            if ctr == 0:
-                return j
-    raise ("error: ", expr, i, expr[i])
-
 
 
 def calc2(expr):
@@ -71,20 +52,12 @@ def calc2(expr):
     while i < len(expr):
         c = expr[i]
         if c == "+":
-            if expr[i-1] == ")":
-                mil = find_matching_left(expr, i-1) + 1
-            else:
-                mil = i-1
-            if expr[i+1] == "(":
-                mir = find_matching_right(expr, i + 1) + 1
-            else:
-                mir = i+2
+            mil = find_matching(expr, i-1, ")") + 1 if expr[i-1] == ")" else i-1
+            mir = find_matching(expr, i + 1, "(") + 1 if expr[i+1] == "(" else  i+2
             expr = expr[:mil] + ["("] + expr[mil:mir] + [")"] + expr[mir:]
             i += 1
         i += 1
     return calc1(expr)
 
-s2 = 0
-for line in m:
-    s2 += calc2(list(line.replace(" ", "")))
+s2 = sum([calc2(list(line.replace(" ", ""))) for line in m])
 print("2: ", s2)
