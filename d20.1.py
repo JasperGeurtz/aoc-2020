8f64ab1f331bfdd82ee6b03c63e45dee7c8071fa
Jasper Geurtz <jcgeurtz@gmail.com>
2020-12-20 12:46:24 +0100

    day20 trying 1

--- d20.1.py
+++ d20.1.py
@@ -46,14 +46,13 @@ def check_borders(image, jimage):
     n = len(image)
     # tuple = (x, y, r, f)
     prev = []
-
     for rot in range(4):
         rot_jimage = copy(jimage)
         jimage = copy(jimage)
         for fdim in range(3):
             if jimage in prev:
                 continue
-            prev.append(jimage)
+            prev.append(copy(jimage))
             if all([image[0][i] == jimage[n - 1][i] for i in range(n)]):
                 border_dirs.append((0, -1, rot, fdim))
             if all([image[n - 1][i] == jimage[0][i] for i in range(n)]):
@@ -66,19 +65,22 @@ def check_borders(image, jimage):
 
         jimage = rotate(copy(rot_jimage))
 
+
     return border_dirs
 
 
 ids = list(images.keys())
 neighbours = {}
-for i, id in enumerate(ids):
+for id in ids:
     image = images[id]
+    print(id)
+    # display(image)
     neighbours[id] = []
-    for j, jid in enumerate(ids):
-        if i == j:
+    for jid in ids:
+        if id == jid:
             continue
         jimage = images[jid]
-        border_dirs = check_borders(image, jimage)
+        border_dirs = check_borders(copy(image), copy(jimage))
         if border_dirs:
             neighbours[id].append((jid, border_dirs))
         # print(id, jid, border_dirs)
